eb7555220a4917bd4c147622682f1e8aaf160a61
estebanvasquex <estebanvasquex@gmail.com>
2023-07-23 00:10:20 -0500

    Se crea una tarjeta de ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,13 @@ function App() {
       <button className="button-secondary">prueba</button>
       <button className="button-primary">prueba</button>
       <button className="button-secondary">prueba</button>
+
+      <div className="product-card">
+        <img src="https://www.un.org/pga/73/wp-content/uploads/sites/53/2018/09/Dummy-image-1.jpg" alt="" />
+        <h1>hola</h1>
+        <h2>hola</h2>
+        <input type="text" />
+      </div>
     </>
   );
 }
